17583d8bd548ba97351565e451cf156c74aa06cd
Ivo Toman <ivorius@gmail.com>
2017-12-16 10:15:41 +0100

    run tests with autoloading from composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,5 @@
 <?php
-require __DIR__ . '/../../../../../vendor/autoload.php';
-
-//require_once __DIR__ . '/../src/IGenerator.php';
-//require_once __DIR__ . '/../src/IWriter.php';
-//require_once __DIR__ . '/../src/IItem.php';
-//require_once __DIR__ . '/../src/Item.php';
-//require_once __DIR__ . '/../src/Heureka/AvailGenerator.php';
-//require_once __DIR__ . '/../src/Heureka/AvailItem.php';
-//require_once __DIR__ . '/../src/XmlWriter.php';
-//require_once __DIR__ . '/../src/XmlElement.php';
+require __DIR__ . '/../../../autoload.php';
 
 
 Tester\Environment::setup();
@@ -16,8 +7,3 @@ date_default_timezone_set('Europe/Prague');
 // create temporary directory
 define('TEMP_DIR', __DIR__ . '/temp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
 Tester\Helpers::purge(TEMP_DIR);
-
-$loader = new Nette\Loaders\RobotLoader;
-$loader->addDirectory(__DIR__ . '/../src');
-$loader->setTempDirectory( __DIR__ . '/temp/');
-$loader->register();
